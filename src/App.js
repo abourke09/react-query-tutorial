5db9051926a052ddc247db89a0d6ff66ff0af8aa
anniebourke <abourke@revacomm.com>
2021-06-03 13:42:04 -1000

    setting up app for react query tutorial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Star Wars Info</h1>git pus
+      <Navbar />
+      <div className={"content"}>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
